e36a664c662c4a98d583e084dc820c444b637a82
Anaet Hossain <anaethoss@gmail.com>
2018-01-06 05:10:21 +0600

    Selected UI Elements 🛠 event listener

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
-let budgetController = (function() {
+const budgetController = (function() {
 
   
 
 })();
 
-let UIController = (function() {
+const UIController = (function() {
+  const DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    addBtn: '.add__btn'
+  };
 
+  return {
 
-});
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, // INC | EXP
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
 
-let controller = (function(budgetCtrl, UICtrl) {
-  
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
 
+  };
+})();
+
+const controller = (function(budgetCtrl, UICtrl) {
+  const DOM = UICtrl.getDOMStrings();
+
+  const addItemCtrl = function() {
+    // 1. Get the filed input data
+    const input = UICtrl.getInput();
+    console.log(input);
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+    
+  }
+
+  document.querySelector(DOM.addBtn).addEventListener('click', addItemCtrl);
+
+  document.addEventListener('keypress', function(evt) {
+    if(evt.keyCode === 13 || evt.which === 13) {
+      addItemCtrl();
+    }
+  });
 
 })(budgetController, UIController);
