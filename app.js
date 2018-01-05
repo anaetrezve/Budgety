a3b5a050e676e1a7c7585defb1c7a190607899c4
Anaet Hossain <anaethoss@gmail.com>
2018-01-06 03:01:38 +0600

    Module pettern syntax added

---
+++ app.js
@@ -0,0 +1,16 @@
+let budgetController = (function() {
+
+  
+
+})();
+
+let UIController = (function() {
+
+
+});
+
+let controller = (function(budgetCtrl, UICtrl) {
+  
+
+
+})(budgetController, UIController);
